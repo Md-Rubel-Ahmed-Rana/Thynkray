679613e513e5a2447da0e591d606453f38b1c391
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-23 15:22:04 +0600

    comment CRUD operation completed

--- backend/src/comment/dto/create-comment.dto.ts
+++ backend/src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCommentDto {}
+
+export class CreateCommentDto {
+    postId: string;
+    userId: string;
+    content?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+
+    constructor(
+        postId: string,
+        userId: string,
+        content?: string,
+        createdAt?: Date,
+        updatedAt?: Date
+    ) {
+        this.postId = postId;
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = createdAt || new Date();
+        this.updatedAt = updatedAt || new Date();
+    }
+}
