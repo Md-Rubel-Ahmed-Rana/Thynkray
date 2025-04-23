f2a00f6d1b0e4f4ba4910b6e8cf260ea2856375e
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-23 15:22:04 +0600

    comment CRUD operation completed

--- backend/src/comment/dto/update-comment.dto.ts
+++ backend/src/comment/dto/update-comment.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCommentDto } from './create-comment.dto';
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+
+export class UpdateCommentDto extends PartialType(CreateCommentDto) {
+    id: string;
+    postId: string;
+    userId: string;
+    content?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+
+    constructor(
+        id: string,
+        postId: string,
+        userId: string,
+        content?: string,
+        createdAt?: Date,
+        updatedAt?: Date
+    ) {
+        super(postId, userId, content, createdAt, updatedAt);
+        this.id = id;
+        this.postId = postId;
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = createdAt || new Date();
+        this.updatedAt = updatedAt || new Date();
+    }
+}
