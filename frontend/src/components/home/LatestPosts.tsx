5ae85f9dbee4f82fbf51fb82021288582487ef37
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-26 23:05:10 +0600

    Custom cursor and smooth scrolling implemented

---
+++ frontend/src/components/home/LatestPosts.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const LatestPosts = () => {
+  return (
+    <Box style={{ minHeight: "100vh" }}>
+      <h2>Latest Posts</h2>
+    </Box>
+  );
+};
+
+export default LatestPosts;
