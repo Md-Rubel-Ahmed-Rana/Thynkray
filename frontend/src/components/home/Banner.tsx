60143f9acfa24a212ef1c8b3d8c589beb3d1438e
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-26 23:05:10 +0600

    Custom cursor and smooth scrolling implemented

---
+++ frontend/src/components/home/Banner.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Banner = () => {
+  return (
+    <Box style={{ minHeight: "100vh" }}>
+      <h1>Hello I am Banner</h1>
+    </Box>
+  );
+};
+
+export default Banner;
