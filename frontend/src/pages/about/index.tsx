07e256784d56a59dfbd9f36168b126b76e371bae
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-29 23:52:24 +0600

    About page design and development completed

---
+++ frontend/src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import AboutUs from "@/components/about";
+import SEOHead from "@/components/common/SEOHead";
+
+const AboutPage = () => {
+  return (
+    <>
+      <SEOHead title={`Authors - Thynkray}`} />
+      <AboutUs />
+    </>
+  );
+};
+
+export default AboutPage;
