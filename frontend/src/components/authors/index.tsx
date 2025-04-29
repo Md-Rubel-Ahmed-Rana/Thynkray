5054f31e31ef92505ff38f15595aef72b3045ba0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-04-29 22:46:37 +0600

    Authors route and page define done

---
+++ frontend/src/components/authors/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Authors = () => {
+  return (
+    <Box>
+      <Typography>Authors</Typography>
+    </Box>
+  );
+};
+
+export default Authors;
