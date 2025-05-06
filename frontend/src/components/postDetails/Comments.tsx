b8fbc2c37e3aa5735ee6f2e82cf521120726f97e
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-06 12:16:34 +0600

    Get comments by post id API integration done

--- frontend/src/components/postDetails/Comments.tsx
+++ frontend/src/components/postDetails/Comments.tsx
@@ -1,4 +1,3 @@
-import { comments } from "@/constants/comments";
 import { Box, Typography, Avatar, Paper, Stack, Button } from "@mui/material";
 import moment from "moment";
 import { useState } from "react";
@@ -17,8 +16,7 @@ const Comments = ({ postId }: Props) => {
   const [isComment, setIsComment] = useState(false);
   const [shouldLogin, setShouldLogin] = useState(false);
 
-  const { comments: data, isLoading } = useGetCommentsByPostId(postId);
-  console.log({ isLoading, comments: data });
+  const { comments, isLoading } = useGetCommentsByPostId(postId);
 
   const handleOpenCommentModal = () => {
     if (user?.id) {
@@ -32,7 +30,7 @@ const Comments = ({ postId }: Props) => {
     <Box>
       <Box display={"flex"} gap={2} mb={2}>
         <Typography variant="h6">
-          {comments.length} Comment{comments.length !== 1 ? "s" : ""}
+          {comments.length} Comment{comments.length > 1 ? "s" : ""}
         </Typography>
         <Button
           onClick={handleOpenCommentModal}
@@ -47,39 +45,49 @@ const Comments = ({ postId }: Props) => {
       {isLoading ? (
         <CommentLoadingSkeleton />
       ) : (
-        <Stack spacing={2}>
-          {comments.map((comment) => (
-            <Paper
-              key={comment.id}
-              elevation={1}
-              sx={{
-                p: 2,
-                borderRadius: 2,
-                display: "flex",
-                gap: 2,
-                alignItems: "flex-start",
-                backgroundColor: "background.paper",
-              }}
-            >
-              <Avatar
-                src={comment.user.profile_image}
-                alt={comment.user.name}
-                sx={{ width: 48, height: 48 }}
-              />
-              <Box>
-                <Typography fontWeight={600}>{comment.user.name}</Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ mb: 1 }}
+        <>
+          {comments.length <= 0 ? (
+            <Typography color="textSecondary">
+              No one comment yet. Be first commenter!
+            </Typography>
+          ) : (
+            <Stack spacing={2}>
+              {comments.map((comment) => (
+                <Paper
+                  key={comment.id}
+                  elevation={1}
+                  sx={{
+                    p: 2,
+                    borderRadius: 2,
+                    display: "flex",
+                    gap: 2,
+                    alignItems: "flex-start",
+                    backgroundColor: "background.paper",
+                  }}
                 >
-                  {moment(new Date(comment.createdAt)).fromNow()}
-                </Typography>
-                <Typography variant="body1">{comment.content}</Typography>
-              </Box>
-            </Paper>
-          ))}
-        </Stack>
+                  <Avatar
+                    src={comment.user.profile_image}
+                    alt={comment.user.name}
+                    sx={{ width: 48, height: 48 }}
+                  />
+                  <Box>
+                    <Typography fontWeight={600}>
+                      {comment.user.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{ mb: 1 }}
+                    >
+                      {moment(new Date(comment.createdAt)).fromNow()}
+                    </Typography>
+                    <Typography variant="body1">{comment.content}</Typography>
+                  </Box>
+                </Paper>
+              ))}
+            </Stack>
+          )}
+        </>
       )}
 
       {isComment && (
