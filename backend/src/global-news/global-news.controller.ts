6cb65bad3b5ed5b471de85afcf502aa8f43a705f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-12 20:49:03 +0600

    International news swagger response api design done

--- backend/src/global-news/global-news.controller.ts
+++ backend/src/global-news/global-news.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { GlobalNewsService } from './global-news.service';
-import { GetGlobalNewsDto } from './dto/get-global-new.dto';
+import { GetGlobalNewsDto, GlobalNewsResponseDto } from './dto/get-global-new.dto';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('global-news')
 export class GlobalNewsController {
   constructor(private readonly globalNewsService: GlobalNewsService) {}
 
+  @ApiOkResponse({
+    description: "Find international news from external api: newsapi",
+    type: GlobalNewsResponseDto
+  })
   @Get()
   findAll(@Query() query: GetGlobalNewsDto) {
     return this.globalNewsService.findAll(query);
