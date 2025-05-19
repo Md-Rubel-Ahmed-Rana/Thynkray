9abf4b839ef1fde50c17016c37cc4136e703fb92
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-19 23:52:57 +0600

    chat AI route and page define done

---
+++ frontend/src/components/chat/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from "@mui/material";
+
+const ChatAI = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Typography>AI Chat is coming soon...</Typography>
+    </Box>
+  );
+};
+
+export default ChatAI;
