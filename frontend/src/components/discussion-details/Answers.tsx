d65a5cadf971111932d7744edc481f3b3c5204e7
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-20 10:51:20 +0600

    Answer create form design done

--- frontend/src/components/discussion-details/Answers.tsx
+++ frontend/src/components/discussion-details/Answers.tsx
@@ -1,8 +1,9 @@
 import { Answer } from "@/modules/discussion/types";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import AnswerHeader from "./AnswerHeader";
 import AnswerCard from "./AnswerCard";
 import { useState } from "react";
+import AnswerAddForm from "./AnswerAddForm";
 
 type Props = {
   answers: Answer[];
@@ -19,11 +20,16 @@ const Answers = ({ answers }: Props) => {
       }}
     >
       <AnswerHeader answers={answers} setSortedAnswers={setSortedAnswers} />
-      <Box>
-        {sortedAnswers.map((answer) => (
-          <AnswerCard answer={answer} key={answer?.id} />
-        ))}
-      </Box>
+      {sortedAnswers.length <= 0 ? (
+        <Typography variant="button">Nobody has answered yet.</Typography>
+      ) : (
+        <Box>
+          {sortedAnswers.map((answer) => (
+            <AnswerCard answer={answer} key={answer?.id} />
+          ))}
+        </Box>
+      )}
+      <AnswerAddForm />
     </Box>
   );
 };
