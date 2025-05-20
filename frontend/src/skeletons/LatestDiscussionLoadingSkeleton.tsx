e6f623d6579a6ab7bcb354b6a19214f6a8923d3d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-20 18:27:12 +0600

    Latest discussions section design completed and used some important places including home, article, post detials etc

---
+++ frontend/src/skeletons/LatestDiscussionLoadingSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Box, Skeleton } from "@mui/material";
+
+const LatestDiscussionLoadingSkeleton = () => {
+  return (
+    <Box>
+      <Skeleton />
+    </Box>
+  );
+};
+
+export default LatestDiscussionLoadingSkeleton;
