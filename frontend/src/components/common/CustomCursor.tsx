3104e5b06bb5fe798ed5c6fc996ed1c8a237f997
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-21 09:37:35 +0600

    [Issue:fixed] Dark Mode Navigation Bar and Cursor Design Overlap Issues on Small Devices #87

--- frontend/src/components/common/CustomCursor.tsx
+++ frontend/src/components/common/CustomCursor.tsx
@@ -1,21 +1,29 @@
 import AnimatedCursor from "react-animated-cursor";
+import { useTheme } from "@mui/material/styles";
 
 const CustomCursor = () => {
+  const theme = useTheme();
+  const isDarkMode = theme.palette.mode === "dark";
+
+  const cursorColor = isDarkMode ? "255, 255, 255" : "50, 50, 70";
+
   return (
     <AnimatedCursor
       innerSize={8}
       outerSize={20}
       showSystemCursor={true}
-      color="50, 50, 70"
+      color={cursorColor}
       outerAlpha={0.4}
       innerScale={1}
       outerScale={2}
       outerStyle={{
-        border: "2px solid #323246",
+        border: `2px solid rgb(${cursorColor})`,
         backgroundColor: "transparent",
+        zIndex: 9999,
       }}
       innerStyle={{
-        backgroundColor: "#323246",
+        backgroundColor: `rgb(${cursorColor})`,
+        zIndex: 9999,
       }}
       clickables={[
         "a",
