3d660a1f14643f10fa789f48fcdb4bee6e967bd9
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-22 11:36:59 +0600

    fallback posts search implemented if meilisearch does not work

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,17 +1,17 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { GetPostDto } from './dto/get-post.dto';
-import { RedisCacheService } from 'src/cache/cache.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { EventEmitter2 } from '@nestjs/event-emitter';
-import { sortByCreatedAtDesc } from 'src/utility/sortByCreatedAt';
-import { Post } from '@prisma/client';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { GetPostDto } from "./dto/get-post.dto";
+import { RedisCacheService } from "src/cache/cache.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { sortByCreatedAtDesc } from "src/utility/sortByCreatedAt";
+import { Post } from "@prisma/client";
 
 @Injectable()
 export class PostService {
-  private readonly cacheKey = 'posts';
-  
+  private readonly cacheKey = "posts";
+
   constructor(
     private readonly cache: RedisCacheService,
     private readonly prisma: PrismaService,
@@ -19,116 +19,125 @@ export class PostService {
   ) {}
 
   async create(createPostDto: CreatePostDto) {
-    const {title, slug, tags, content, category, thumbnail, authorId, description} = createPostDto;
-   await this.prisma.post.create({data: {
+    const {
       title,
       slug,
       tags,
+      content,
       category,
       thumbnail,
       authorId,
       description,
-      content: {
-        create: content.map((section) => ({
-          title: section.title,
-          images: section.images,
-          description: section.description,
-        })),  
+    } = createPostDto;
+    await this.prisma.post.create({
+      data: {
+        title,
+        slug,
+        tags,
+        category,
+        thumbnail,
+        authorId,
+        description,
+        content: {
+          create: content.map((section) => ({
+            title: section.title,
+            images: section.images,
+            description: section.description,
+          })),
+        },
       },
-    }});
+    });
 
     const post = await this.prisma.post.findUnique({
       where: {
-        slug
+        slug,
       },
       include: {
         author: true,
         content: true,
-      }
-    })
+      },
+    });
 
-    
     const postDto = GetPostDto.fromEntity(post);
 
     // fire post created event
-    this.eventEmitter.emit('post.created', postDto);
+    this.eventEmitter.emit("post.created", postDto);
 
     return {
-      message: 'Post created successfully!',
+      message: "Post created successfully!",
       statusCode: 201,
-    }
+    };
   }
 
   async findAll() {
-    let postsData = []
-    let message = 'Posts retrieved successfully!'
-    const postsFromCache = await this.cache.get(this.cacheKey)
-    if(postsFromCache !== null && postsFromCache){
-      postsData = sortByCreatedAtDesc(postsFromCache)  
-      message = 'Posts retrieved from cache!'
-    }else{
+    let postsData = [];
+    let message = "Posts retrieved successfully!";
+    const postsFromCache = await this.cache.get(this.cacheKey);
+    if (postsFromCache !== null && postsFromCache) {
+      postsData = sortByCreatedAtDesc(postsFromCache);
+      message = "Posts retrieved from cache!";
+    } else {
       const posts = await this.prisma.post.findMany({
-          include: {
-            author: true,
-            content: true,
-             _count: {
-              select: {
-                comments: true
-              }
-             }
+        include: {
+          author: true,
+          content: true,
+          _count: {
+            select: {
+              comments: true,
+            },
           },
-          orderBy: {
-            createdAt: "desc"
-          }
-        });
-      const postDtos = GetPostDto.fromEntities(posts)
-      postsData = postDtos
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      const postDtos = GetPostDto.fromEntities(posts);
+      postsData = postDtos;
     }
 
     return {
       message,
       data: postsData,
       statusCode: 200,
-    }
+    };
   }
-
 
   async findPopularPosts() {
     const posts = await this.prisma.post.findMany({
       include: {
         author: true,
         content: true,
-          _count: {
+        _count: {
           select: {
-            comments: true
-          }
-          }
+            comments: true,
+          },
+        },
       },
       orderBy: {
         views: "desc",
       },
-      take: 10
+      take: 10,
     });
 
-    const postDtos = GetPostDto.fromEntities(posts)
+    const postDtos = GetPostDto.fromEntities(posts);
 
     return {
       message: "Posts retrieved successfully!",
       data: postDtos,
       success: true,
       statusCode: 200,
-    }
+    };
   }
 
   async findRelatedPosts(currentPostId: string) {
     const currentPost = await this.prisma.post.findUnique({
       where: { id: currentPostId },
     });
-  
+
     if (!currentPost) {
       throw new Error("Post not found");
     }
-  
+
     const relatedPosts = await this.prisma.post.findMany({
       where: {
         id: { not: currentPostId },
@@ -142,14 +151,14 @@ export class PostService {
       },
       take: 10,
     });
-  
-    const relatedPostIds = relatedPosts.map(post => post.id);
-  
+
+    const relatedPostIds = relatedPosts.map((post) => post.id);
+
     let remainingPosts: typeof relatedPosts = [];
-  
+
     if (relatedPosts.length < 10) {
       const needed = 10 - relatedPosts.length;
-  
+
       remainingPosts = await this.prisma.post.findMany({
         where: {
           id: {
@@ -162,9 +171,12 @@ export class PostService {
         take: needed,
       });
     }
-  
-    const finalPostIds = [...relatedPostIds, ...remainingPosts.map(p => p.id)];
-  
+
+    const finalPostIds = [
+      ...relatedPostIds,
+      ...remainingPosts.map((p) => p.id),
+    ];
+
     const enrichedPosts = await this.prisma.post.findMany({
       where: {
         id: { in: finalPostIds },
@@ -179,9 +191,9 @@ export class PostService {
         },
       },
     });
-  
+
     const postDtos = GetPostDto.fromEntities(enrichedPosts);
-  
+
     return {
       message: "Posts retrieved successfully!",
       data: postDtos,
@@ -189,175 +201,172 @@ export class PostService {
       statusCode: 200,
     };
   }
-  
 
   async findAllByAuthorId(authorId: string) {
-
-    const postsFromCache = await this.cache.get(this.cacheKey)
-    if(postsFromCache !== null){
-      const posts = postsFromCache.filter((post: any) => post?.author?.id === authorId)
-      if(posts?.length > 0){
+    const postsFromCache = await this.cache.get(this.cacheKey);
+    if (postsFromCache !== null) {
+      const posts = postsFromCache.filter(
+        (post: any) => post?.author?.id === authorId
+      );
+      if (posts?.length > 0) {
         return {
-          message: 'Posts retrieved from cache!',
-          data: sortByCreatedAtDesc(posts)
-        }
+          message: "Posts retrieved from cache!",
+          data: sortByCreatedAtDesc(posts),
+        };
       }
     }
 
-    const posts =await this.prisma.post.findMany({
+    const posts = await this.prisma.post.findMany({
       where: {
-        authorId: authorId
+        authorId: authorId,
       },
       include: {
         author: true,
         content: true,
         _count: {
           select: {
-            comments: true
-          }
-        }
+            comments: true,
+          },
+        },
       },
       orderBy: {
-        createdAt: "desc"
-      }
-    })
-    const postDtos = GetPostDto.fromEntities(posts)
+        createdAt: "desc",
+      },
+    });
+    const postDtos = GetPostDto.fromEntities(posts);
     return {
       statusCode: 200,
       message: "Posts retrieved successfully",
       data: postDtos,
-    }
+    };
   }
 
   async getLatestPosts(limit = 10) {
     const latestPosts = await this.prisma.post.findMany({
-       include: {
+      include: {
         author: true,
-        content: true
-       },
+        content: true,
+      },
       take: limit,
       orderBy: {
-        createdAt: 'desc',
+        createdAt: "desc",
       },
     });
 
-     const postDtos = GetPostDto.fromEntities(latestPosts)
+    const postDtos = GetPostDto.fromEntities(latestPosts);
 
     return {
       message: "Latest posts fetched successfully!",
       statusCode: 200,
-      data: postDtos
-    }
+      data: postDtos,
+    };
   }
 
   async getPostsByCategory(category: string) {
     const categorizedPosts = await this.prisma.post.findMany({
       where: {
-        category: category
+        category: category,
       },
-       include: {
+      include: {
         author: true,
-        content: true
-       },
+        content: true,
+      },
       orderBy: {
-        createdAt: 'desc',
+        createdAt: "desc",
       },
     });
 
-
-     const postDtos = GetPostDto.fromEntities(categorizedPosts)
+    const postDtos = GetPostDto.fromEntities(categorizedPosts);
 
     return {
       message: "Categorized posts fetched successfully!",
       statusCode: 200,
-      data: postDtos
-    }
+      data: postDtos,
+    };
   }
- 
- async findOne(id: string) {
-    const posts = await this.cache.get(this.cacheKey) as GetPostDto[]
-    const postFromCache = posts.find((post) => post?.id === id)
 
-    if(postFromCache !== null && postFromCache){
+  async findOne(id: string) {
+    const posts = (await this.cache.get(this.cacheKey)) as GetPostDto[];
+    const postFromCache = posts.find((post) => post?.id === id);
+
+    if (postFromCache !== null && postFromCache) {
       return {
-        message: 'Post retrieved from cache successfully!',
+        message: "Post retrieved from cache successfully!",
         data: postFromCache,
-         statusCode: 200,
-      }
+        statusCode: 200,
+      };
     }
 
     const post = await this.prisma.post.findUnique({
       where: {
-        id
+        id,
       },
       include: {
         author: true,
         content: true,
         _count: {
           select: {
-            comments: true
-          }
-        }
-      }
+            comments: true,
+          },
+        },
+      },
     });
-    
+
     if (!post) {
-       throw new HttpException('Post was not found', HttpStatus.NOT_FOUND);
+      throw new HttpException("Post was not found", HttpStatus.NOT_FOUND);
     }
-    
+
     const postDto = GetPostDto.fromEntity(post);
 
     return {
-      message: 'Post retrieved successfully!',
+      message: "Post retrieved successfully!",
       data: postDto,
-       statusCode: 200,
+      statusCode: 200,
     };
   }
 
- async findOneBySlug(slug: string) {
-    const posts = await this.cache.get(this.cacheKey) as GetPostDto[]
-    const postFromCache = posts.find((post) => post?.slug === slug)
+  async findOneBySlug(slug: string) {
+    const posts = (await this.cache.get(this.cacheKey)) as GetPostDto[];
+    const postFromCache = posts.find((post) => post?.slug === slug);
 
-    if(postFromCache !== null && postFromCache){
+    if (postFromCache !== null && postFromCache) {
       return {
-        message: 'Post retrieved from cache successfully!',
+        message: "Post retrieved from cache successfully!",
         data: postFromCache,
-         statusCode: 200,
-      }
+        statusCode: 200,
+      };
     }
 
     const post = await this.prisma.post.findUnique({
       where: {
-        slug
+        slug,
       },
       include: {
         author: true,
         content: true,
-      }
+      },
     });
     if (!post) {
-       throw new HttpException('Post was not found', HttpStatus.NOT_FOUND);
+      throw new HttpException("Post was not found", HttpStatus.NOT_FOUND);
     }
 
     const postDto = GetPostDto.fromEntity(post);
 
     return {
-      message: 'Post retrieved successfully!',
+      message: "Post retrieved successfully!",
       data: postDto,
-       statusCode: 200,
+      statusCode: 200,
     };
   }
 
-  
-
   async update(id: string, updatePostDto: UpdatePostDto) {
+    await this.isPostExist(id);
 
-  await this.isPostExist(id)
+    const { title, slug, tags, content, category, thumbnail, description } =
+      updatePostDto;
 
-  const { title, slug, tags, content, category, thumbnail, description } = updatePostDto;
-
-  const oldContent = content.filter(section => section?.id);
-  const newContent = content.filter(section => !section?.id);
+    const oldContent = content.filter((section) => section?.id);
+    const newContent = content.filter((section) => !section?.id);
 
     const post = await this.prisma.post.update({
       where: { id },
@@ -368,11 +377,11 @@ export class PostService {
         category,
         thumbnail,
         description,
-      }
+      },
     });
 
     await Promise.all(
-      oldContent.map(section =>
+      oldContent.map((section) =>
         this.prisma.postSection.update({
           where: { id: section.id },
           data: {
@@ -386,7 +395,7 @@ export class PostService {
 
     if (newContent.length > 0) {
       await this.prisma.postSection.createMany({
-        data: newContent.map(section => ({
+        data: newContent.map((section) => ({
           title: section.title,
           images: section.images,
           description: section.description,
@@ -403,58 +412,57 @@ export class PostService {
     const postDto = GetPostDto.fromEntity(updatedPost);
 
     // fire post updated event
-    this.eventEmitter.emit('post.updated', {...postDto, _old: post });
+    this.eventEmitter.emit("post.updated", { ...postDto, _old: post });
 
     return {
-      message: 'Post updated successfully!',
+      message: "Post updated successfully!",
       statusCode: 200,
     };
   }
 
   async remove(id: string) {
-    await this.isPostExist(id)
+    await this.isPostExist(id);
 
     const post = await this.prisma.post.findUnique({
       where: {
-        id
+        id,
       },
       include: {
         author: true,
         content: true,
-      }
+      },
     });
 
     const postDto = GetPostDto.fromEntity(post);
 
-    this.eventEmitter.emit('post.delete.images', postDto);
+    this.eventEmitter.emit("post.delete.images", postDto);
 
     await this.prisma.post.delete({
       where: {
-        id
-      }
+        id,
+      },
     });
 
     // fire post deleted event
-    this.eventEmitter.emit('post.deleted', id);
+    this.eventEmitter.emit("post.deleted", id);
 
     return {
-      message: 'Post deleted successfully!',
+      message: "Post deleted successfully!",
       statusCode: 200,
-    }
+    };
   }
 
-  async incrementViews(id: string){
-    const isExist = await this.isPostExist(id)
+  async incrementViews(id: string) {
+    const isExist = await this.isPostExist(id);
 
-    const post  = await this.prisma.post.update({
-      where: {id},
+    const post = await this.prisma.post.update({
+      where: { id },
       data: {
-        views: {increment: 1},
-        updatedAt: isExist.updatedAt
+        views: { increment: 1 },
+        updatedAt: isExist.updatedAt,
       },
-      include: { author: true, content: true }
-      
-    })
+      include: { author: true, content: true },
+    });
 
     const updatedPost = await this.prisma.post.findUnique({
       where: { id },
@@ -464,28 +472,27 @@ export class PostService {
     const postDto = GetPostDto.fromEntity(updatedPost);
 
     // fire post updated event
-    this.eventEmitter.emit('post.updated', {...postDto, _old: post });
+    this.eventEmitter.emit("post.updated", { ...postDto, _old: post });
 
     return {
       statusCode: 200,
       success: true,
       message: "Post views incremented successfully",
-      data: null
-    }
+      data: null,
+    };
   }
 
-  async isPostExist(id: string): Promise<Post>{
+  async isPostExist(id: string): Promise<Post> {
     const post = await this.prisma.post.findUnique({
       where: {
-        id
+        id,
       },
     });
 
     if (!post) {
-      throw new HttpException('Post was not found', HttpStatus.NOT_FOUND);
-    }else{
-      return post
+      throw new HttpException("Post was not found", HttpStatus.NOT_FOUND);
+    } else {
+      return post;
     }
   }
-
 }
--- backend/src/search-library/meilisearch.service.ts
+++ backend/src/search-library/meilisearch.service.ts
@@ -5,141 +5,252 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { GetPostDto } from "src/post/dto/get-post.dto";
 import { PinoLogger } from "src/common/logger/pino-logger.service";
 
-
 export class MeiliSearchService {
   private index: any;
 
   constructor(
     private readonly prisma: PrismaService,
-    private readonly logger: PinoLogger,
+    private readonly logger: PinoLogger
   ) {
-    this.index = MeiliSearchClient.index('thynkray-blogs')
+    this.index = MeiliSearchClient.index("thynkray-blogs");
   }
 
-  async configureIndex(): Promise<{statusCode: number, message: string}> {
+  async configureIndex(): Promise<{ statusCode: number; message: string }> {
     try {
-      await this.index.updateSearchableAttributes(["titles" ,"content", "tags", "author", "category", "published"]);
-      await this.index.updateFilterableAttributes(["author", "tags", "published", "category"]);
+      await this.index.updateSearchableAttributes([
+        "titles",
+        "content",
+        "tags",
+        "author",
+        "category",
+        "published",
+      ]);
+      await this.index.updateFilterableAttributes([
+        "author",
+        "tags",
+        "published",
+        "category",
+      ]);
       return {
         statusCode: 201,
         message: "MeiliSearch index configured successfully.",
-      }
+      };
     } catch (error: any) {
-      this.logger.error(`Error configuring MeiliSearch index. Error:${error?.message}`);
+      this.logger.error(
+        `Error configuring MeiliSearch index. Error:${error?.message}`
+      );
     }
   }
 
-  async addAllPostsOnMeilisearch(): Promise<{statusCode: number, message: string, response: any}> {
+  async addAllPostsOnMeilisearch(): Promise<{
+    statusCode: number;
+    message: string;
+    response: any;
+  }> {
     try {
-        const posts =  await this.prisma.post.findMany({
-           include: {
-            author: true,
-            content: true,
-            _count: {
-              select: {
-                comments: true
-              }
-             }
+      const posts = await this.prisma.post.findMany({
+        include: {
+          author: true,
+          content: true,
+          _count: {
+            select: {
+              comments: true,
+            },
+          },
+        },
+      });
+      const postDtos = GetPostDto.fromEntities(posts);
+      const documents = postDtos
+        .map((post) => {
+          try {
+            return MeiliSearchDto.fromPost(post);
+          } catch (e: any) {
+            this.logger.warn(`Skipping post due to error: ${e?.message}`);
+            return null;
           }
         })
-        const postDtos = GetPostDto.fromEntities(posts)
-        const documents = postDtos
-            .map((post) => {
-              try {
-                return MeiliSearchDto.fromPost(post);
-              } catch (e: any) {
-                this.logger.warn(`Skipping post due to error: ${e?.message}`);
-                return null;
-              }
-            })
-            .filter(Boolean);
+        .filter(Boolean);
 
-        const response = await this.index.addDocuments(documents);
-        return {
-          statusCode: 200,
-          message: "Documents added to MeiliSearch",
-          response
-        }
+      const response = await this.index.addDocuments(documents);
+      return {
+        statusCode: 200,
+        message: "Documents added to MeiliSearch",
+        response,
+      };
     } catch (error: any) {
-      this.logger.error(`Error adding full documents to MeiliSearch. Error: ${error?.message}`);
+      this.logger.error(
+        `Error adding full documents to MeiliSearch. Error: ${error?.message}`
+      );
     }
   }
 
-  async search(searchText: string, filters: string[] = []): Promise<{statusCode: number, message: string, data: any}> {
+  async search(
+    searchText: string,
+    filters: string[] = []
+  ): Promise<{ statusCode: number; message: string; data: any[] }> {
     try {
       const response = await this.index.search(searchText, {
         filter: filters,
-        attributesToHighlight: ['title', "titles", "description", 'content'],
-        attributesToRetrieve: ['id', "thumbnail", 'title', "slug", 'description', 'tags', 'author', 'category', 'published'],
+        attributesToHighlight: ["title", "titles", "description", "content"],
+        attributesToRetrieve: [
+          "id",
+          "thumbnail",
+          "title",
+          "slug",
+          "description",
+          "tags",
+          "author",
+          "category",
+          "published",
+        ],
       });
+
+      if (response?.hits?.length) {
+        return {
+          message: "Posts retrieved successfully from Meilisearch!",
+          statusCode: 200,
+          data: response.hits,
+        };
+      }
+
+      this.logger.warn(
+        "No results from Meilisearch. Falling back to Prisma DB search."
+      );
+      const fallbackResults = await this.prismaFallbackSearch(
+        searchText,
+        filters
+      );
       return {
-      message: 'Posts retrieved successfully!',
-      statusCode: 200,
-      data: response?.hits
-    }
+        message: "Posts retrieved from DB fallback.",
+        statusCode: 200,
+        data: fallbackResults,
+      };
     } catch (error: any) {
-      this.logger.error(`Error searching in MeiliSearch. Error: ${error?.message}`);
+      this.logger.error(
+        `Error searching in Meilisearch. Error: ${error?.message}`
+      );
+      // Fallback if Meilisearch fails
+      const fallbackResults = await this.prismaFallbackSearch(
+        searchText,
+        filters
+      );
+      return {
+        message: "Posts retrieved from DB fallback after Meilisearch error.",
+        statusCode: 200,
+        data: fallbackResults,
+      };
     }
   }
 
-  async getAllPosts(): Promise<{statusCode: number, message: string ,data: GetPostDto[]}>{
-    const posts = await this.index.getDocuments({ limit: 10000 })
+  async getAllPosts(): Promise<{
+    statusCode: number;
+    message: string;
+    data: GetPostDto[];
+  }> {
+    const posts = await this.index.getDocuments({ limit: 10000 });
     return {
       statusCode: 200,
       message: "All the posts retrieved from meilisearch",
-      data: posts?.results
-    }
+      data: posts?.results,
+    };
   }
 
   async updatePosts(posts: any[]) {
-    const postDtos = GetPostDto.fromEntities(posts)
-    const documents = postDtos.map((post) => MeiliSearchDto.fromPost(post))
+    const postDtos = GetPostDto.fromEntities(posts);
+    const documents = postDtos.map((post) => MeiliSearchDto.fromPost(post));
     return this.index.addDocuments(documents);
   }
-  
+
   async deletePosts(ids: string[]) {
     return this.index.deleteDocuments(ids);
   }
 
-  async deleteFullDocuments(){
-    const response = await this.index.deleteAllDocuments()
+  async deleteFullDocuments() {
+    const response = await this.index.deleteAllDocuments();
     return {
       statusCode: 200,
       message: "All the documents deleted from meilisearch",
-      data: response
+      data: response,
+    };
+  }
+
+  private async prismaFallbackSearch(
+    searchText: string,
+    filters: string[]
+  ): Promise<MeiliSearchDto[]> {
+    const whereClause: any = {
+      OR: [
+        { title: { contains: searchText, mode: "insensitive" } },
+        { description: { contains: searchText, mode: "insensitive" } },
+        { content: { contains: searchText, mode: "insensitive" } },
+      ],
+    };
+
+    if (filters.length > 0) {
+      whereClause.AND = filters.map((f) => ({
+        tags: {
+          has: f,
+        },
+      }));
     }
+
+    const posts = await this.prisma.post.findMany({
+      where: whereClause,
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        description: true,
+        tags: true,
+        author: true,
+        category: true,
+        createdAt: true,
+        thumbnail: true,
+      },
+    });
+    const postDtos = GetPostDto.fromEntities(posts);
+    const documents = postDtos
+      .map((post) => {
+        try {
+          return MeiliSearchDto.fromPost(post);
+        } catch (e: any) {
+          this.logger.warn(`Skipping post due to error: ${e?.message}`);
+          return null;
+        }
+      })
+      .filter(Boolean);
+
+    return documents;
   }
 
-
-  @OnEvent('post.created')
-  async postCreatedEvent(newPost: GetPostDto){
+  @OnEvent("post.created")
+  async postCreatedEvent(newPost: GetPostDto) {
     const response = await this.index.addDocuments([newPost]);
-      console.log({
-        message: "New post added to MeiliSearch",
-        data: newPost,
-        response
-      });
+    console.log({
+      message: "New post added to MeiliSearch",
+      data: newPost,
+      response,
+    });
   }
 
-  @OnEvent('post.updated')
-  async postUpdatedEvent(updatedPost: any){
-    delete updatedPost?._old
-     const response = await this.index.updateDocuments([updatedPost]);
-      console.log({
-        message: "Post updated to MeiliSearch",
-        data: updatedPost,
-        response
-      });
+  @OnEvent("post.updated")
+  async postUpdatedEvent(updatedPost: any) {
+    delete updatedPost?._old;
+    const response = await this.index.updateDocuments([updatedPost]);
+    console.log({
+      message: "Post updated to MeiliSearch",
+      data: updatedPost,
+      response,
+    });
   }
 
-  @OnEvent('post.deleted')
-  async postDeletedEvent(id: string){
-    const response = this.index.deleteDocument(id)
-      console.log({
-        message: "Post deleted from MeiliSearch",
-        data: {id},
-        response
-      });
+  @OnEvent("post.deleted")
+  async postDeletedEvent(id: string) {
+    const response = this.index.deleteDocument(id);
+    console.log({
+      message: "Post deleted from MeiliSearch",
+      data: { id },
+      response,
+    });
   }
 }
-
