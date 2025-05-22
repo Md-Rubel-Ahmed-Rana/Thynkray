f824018459cb741ddc6645265745a93655fe713e
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-22 11:22:36 +0600

    Chat list, messages, and chat id send to frontend sucessfully

--- backend/src/search-library/meilisearch.controller.ts
+++ backend/src/search-library/meilisearch.controller.ts
@@ -6,40 +6,39 @@ import { MeilisearchResponseDto } from "./meilisearch.dto";
 
 @Controller("meilisearch")
 export class MeilisearchController {
-    constructor(private readonly meilisearchService: MeiliSearchService){}
+  constructor(private readonly meilisearchService: MeiliSearchService) {}
 
-
-    @ApiOkResponse({
-        description: "Search posts from meilisearch index database",
-        type: MeilisearchResponseDto
-    })
-    @Get("/posts/search")
-    search(@Query("q") q: string, @Query("filters") filters: string) {
+  @ApiOkResponse({
+    description: "Search posts from meilisearch index database",
+    type: MeilisearchResponseDto,
+  })
+  @Get("/posts/search")
+  search(@Query("q") q: string, @Query("filters") filters: string) {
     const parsedFilters = buildMeiliSearchFilters(filters);
-        return this.meilisearchService.search(q, parsedFilters);
-    }
+    return this.meilisearchService.search(q, parsedFilters);
+  }
 
-    @ApiExcludeEndpoint()
-    @Get("configure/index/posts")
-    async configureIndex(){
-        return this.meilisearchService.configureIndex()
-    }
+  @ApiExcludeEndpoint()
+  @Get("configure/index/posts")
+  async configureIndex() {
+    return this.meilisearchService.configureIndex();
+  }
 
-    @ApiExcludeEndpoint()
-    @Get("add/posts")
-    async addAllPostsOnMeilisearch(){
-        return this.meilisearchService.addAllPostsOnMeilisearch()
-    }
+  @ApiExcludeEndpoint()
+  @Get("add/posts")
+  async addAllPostsOnMeilisearch() {
+    return this.meilisearchService.addAllPostsOnMeilisearch();
+  }
 
-    @ApiExcludeEndpoint()
-    @Get("/posts/get-all")
-    getAllPosts() {
-        return this.meilisearchService.getAllPosts();
-    }
-    
-    @ApiExcludeEndpoint()
-    @Get("/posts/delete-all")
-    deleteFullDocuments() {
-        return this.meilisearchService.deleteFullDocuments();
-    }
+  @ApiExcludeEndpoint()
+  @Get("/posts/get-all")
+  getAllPosts() {
+    return this.meilisearchService.getAllPosts();
+  }
+
+  @ApiExcludeEndpoint()
+  @Get("/posts/delete-all")
+  deleteFullDocuments() {
+    return this.meilisearchService.deleteFullDocuments();
+  }
 }
